efb2d5c8f6f67e20dfd65c29a0e56b525909c8a1
desertkun <desertkun@gmail.com>
2017-08-24 23:39:51 +0200

    Updated default values for less tuning for dev environment

--- src/options.py
+++ src/options.py
@@ -4,12 +4,12 @@ from common.options import define
 # Main
 
 define("host",
-       default="http://profile-dev.anthill",
+       default="http://localhost:9512",
        help="Public hostname of this service",
        type=str)
 
 define("listen",
-       default="port:10200",
+       default="unix:/usr/local/var/run/anthill/dev_profile.sock",
        help="Socket to listen. Could be a port number (port:N), or a unix domain socket (unix:PATH)",
        type=str)
 
@@ -26,7 +26,7 @@ define("db_host",
        help="MySQL database location")
 
 define("db_username",
-       default="anthill",
+       default="root",
        type=str,
        help="MySQL account username")
 
@@ -36,6 +36,6 @@ define("db_password",
        help="MySQL account password")
 
 define("db_name",
-       default="profile",
+       default="dev_profile",
        type=str,
        help="MySQL database name")
